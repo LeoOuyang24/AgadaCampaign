3ccce3db0bc47a1a4fc51f9384385cf1598ac621
leo ouyang <Leo Ouyang>
2024-07-28 11:27:03 -0700

    several new locations

--- icons.js
+++ icons.js
@@ -1,10 +1,41 @@
 
+export function getIcon(type)
+{
+	switch (type)
+	{
+	case "kingdom":
+	case "Kingdom":
+		return kingIcon
+	case "POI":
+	case "Point of Interest":
+		return POIIcon
+	case "village":
+	case "Village":
+		return villageIcon
+	case "Town":
+	case "City":
+		return townIcon
+	}
+}
+
+
 export let POIIcon = L.icon({
     iconUrl: 'sprites/icons/POI.png',
-    iconSize: [20,30]
+    iconSize: [16,20]
 });
 
 export let kingIcon = L.icon({
 	iconUrl: 'sprites/icons/kingdom.png',
-	iconSize: [20,15]
+	iconSize: [40,30]
+})
+
+export let villageIcon = L.icon({
+	iconUrl: 'sprites/icons/village.png',
+	iconSize: [20,20]
+})
+
+let townIcon = L.icon({
+	iconUrl: 'sprites/icons/town.png',
+	iconSize: [25,25]
+
 })
--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-import * as icons from "./icons.js";
 import {sidebar} from "./sidebar.js"
+import {loadMarkers} from "./markers.js"
+
+export const width = 1000
+export const height = 1000
 
 var map = L.map('map', {
     crs: L.CRS.Simple
    	});
-var width = 1000
-var height = 1000
 var bounds = [[0,0], [width,height]];
 var image = L.imageOverlay('sprites/map.png', bounds).addTo(map);
 map.fitBounds(bounds);
 map.setMaxBounds(bounds)
 
-const marker = L.marker([.55*height, .73*width],{icon: icons.kingIcon}).addTo(map)
+map.on("click",(e) => document.getElementById("sidebar").style.display = "none")
 
-
-function onMapClick(e) {
-    let style = document.getElementById("sidebar").style
-    style.display = (style.display == null || style.display != "none" ? "none" : "initial") ;
-}
-
-marker.on('click', onMapClick);
 sidebar.addTo(map)
+
+loadMarkers(map)
